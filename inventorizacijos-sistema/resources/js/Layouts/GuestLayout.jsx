47a139d39def2a962ae674e27ce7224861fdd336
eStukas <ernestastukas@gmail.com>
2024-12-15 21:23:16 +0200

    Updated the Guest layout to contain certain stylistic elements

--- inventorizacijos-sistema/resources/js/Layouts/GuestLayout.jsx
+++ inventorizacijos-sistema/resources/js/Layouts/GuestLayout.jsx
@@ -3,14 +3,14 @@ import { Link } from '@inertiajs/react';
 
 export default function GuestLayout({ children }) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
+        <div className="GuestLayout flex min-h-screen flex-col items-center pt-6 sm:justify-center sm:pt-0">
             <div>
                 <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
+                    <img src="/images/kitm_logo_v1.png" alt="KITM_LOGO" />
                 </Link>
             </div>
 
-            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg border-[1px] border-black">
                 {children}
             </div>
         </div>
