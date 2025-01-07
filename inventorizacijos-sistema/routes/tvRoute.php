d50a92752796ddbf1dfa4af44547b500ca9d0e38
eStukas <ernestastukas@gmail.com>
2025-01-07 19:53:47 +0200

    Fixed route rules for specific roles

--- inventorizacijos-sistema/routes/tvRoute.php
+++ inventorizacijos-sistema/routes/tvRoute.php
@@ -1,23 +1,25 @@
 <?php
 use App\Http\Controllers\TVController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\IsAdmin;
 
-Route::get('/tv-control', [TVController::class, 'index'])->name('tv.index'); // List TVs
+/*Displaying the list of TVs */
+Route::get('/tv-control', [TVController::class, 'index'])->name('tv.index');
 
-
-/*Creating and storing new tv data */
-Route::get('/tv-control/create', [TVController::class, 'create'])->name('tv.create');
-Route::post('/tv-control/create', [TVController::class, 'store'])->name('tv.store');
-
-/*Displaying specified tv data not needed as of now*/
-Route::get('/tv-control/view/{id}', [TVController::class, 'show'])->name('tv.show');
-
-/*Editing and updating existing TV data */
-Route::get('/tv-control/edit/{tv}', [TVController::class, 'edit'])->name('tv.edit');
-Route::put('/tv-control/edit/{tv}', [TVController::class, 'update'])->name('tv.update');
-
-/*Deleting TV data */
-Route::delete('/tv-control/{tv}', [TVController::class, 'destroy'])->name('tv.delete');
-
-Route::get('/tv-control/ping/{tv}', [TVController::class, 'ping'])->name('tv.ping');
-
+Route::middleware(['auth', IsAdmin::class])->group(function(){
+    /*Creating and storing new tv data */
+    Route::get('/tv-control/create', [TVController::class, 'create'])->name('tv.create');
+    Route::post('/tv-control/create', [TVController::class, 'store'])->name('tv.store');
+    
+    /*Displaying specified tv data not needed as of now*/
+    Route::get('/tv-control/view/{id}', [TVController::class, 'show'])->name('tv.show');
+    
+    /*Editing and updating existing TV data */
+    Route::get('/tv-control/edit/{tv}', [TVController::class, 'edit'])->name('tv.edit');
+    Route::put('/tv-control/edit/{tv}', [TVController::class, 'update'])->name('tv.update');
+    
+    /*Deleting TV data */
+    Route::delete('/tv-control/{tv}', [TVController::class, 'destroy'])->name('tv.delete');
+    
+    Route::get('/tv-control/ping/{tv}', [TVController::class, 'ping'])->name('tv.ping');
+});    
