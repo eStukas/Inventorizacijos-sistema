9691978aadf6582e957afe1417d893db18ac34ce
eStukas <ernestastukas@gmail.com>
2025-01-07 04:06:58 +0200

    Experimenting with route rules for specific roles (ask bossman for help)

--- inventorizacijos-sistema/routes/booksRoute.php
+++ inventorizacijos-sistema/routes/booksRoute.php
@@ -4,11 +4,18 @@ use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*Displaying the list of book */
+
 Route::get('/book', [BookController::class, 'index'])->name('book.index');
 
-/*Creating and storing new book data */
+// Route::middleware(['auth', 'role:1'])->group(function () {
+//     /*Creatin and storing new book*/
+//     Route::get('/book/create', [BookController::class, 'create'])->name('book.create');
+//     Route::post('/book/create', [BookController::class, 'store'])->name('book.store');
+// });
+
 Route::get('/book/create', [BookController::class, 'create'])->name('book.create');
 Route::post('/book/create', [BookController::class, 'store'])->name('book.store');
+
 
 /*Displaying specified book data not needed as of now*/
 Route::get('/book/view/{id}', [BookController::class, 'show'])->name('book.show');
@@ -18,7 +25,5 @@ Route::get('/book/edit/{book}', [BookController::class, 'edit'])->name('book.edi
 Route::post('/book/edit/{book}', [BookController::class, 'update'])->name('book.update');
 
 // /*Deleting book data *
-// Route::get('/book/destroy/{id}', [BookController::class, 'destroy'])->name('book.delete');
-
 
 Route::delete('/book/{book}', [BookController::class, 'destroy'])->name('book.destroy');
--- inventorizacijos-sistema/routes/web.php
+++ inventorizacijos-sistema/routes/web.php
@@ -28,7 +28,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::middleware(['checkRole:1'])->get('/test-role', function () {
+    return 'Middleware is working!';
+});
 
 
 require __DIR__.'/auth.php';
